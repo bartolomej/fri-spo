07fef92578b824288e27f2e765e751216ca5ea49
Bart <bartolomej.kozorog@gmail.com>
2024-10-17 18:25:29 +0200

    fix empty file case

--- rep.c
+++ rep.c
@@ -31,13 +31,18 @@ int main(const int argc, char* argv[]) {
     FILE *input_file = fopen(input_file_path, "r");
     const int input_file_fd = fileno(input_file);
     off_t offset_from_end = -1;
-    off_t offset_from_start = lseek(input_file_fd, offset_from_end, SEEK_END);
 
     int seen_newlines = 0;
     while (1) {
+        const off_t offset_from_start = lseek(input_file_fd, offset_from_end, SEEK_END);
+        if (offset_from_start <= 0) {
+            // We haven't seen the specified number of newlines,
+            // but are already at the beginning of the file.
+            // This also handles the case of empty files.
+            break;
+        }
         char input;
-        const ssize_t bytes_read = read(input_file_fd, &input, 1);
-        if (bytes_read == -1) {
+        if (read(input_file_fd, &input, 1) == -1) {
             printf("Error reading bytes from input file\n");
             exit(1);
         }
@@ -48,13 +53,8 @@ int main(const int argc, char* argv[]) {
         if (seen_newlines == lines_to_read) {
             break;
         }
+        // Step towards the start of the file
         offset_from_end -= 1;
-        offset_from_start = lseek(input_file_fd, offset_from_end, SEEK_END);
-        if (offset_from_start == 0) {
-            // We haven't seen the specified number of newlines,
-            // but are already at the beginning of the file.
-            break;
-        }
     }
 
     const unsigned long output_buffer_size = (-offset_from_end) * sizeof(char);
